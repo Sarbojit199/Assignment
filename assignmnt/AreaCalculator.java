2dde24123848d41762d5d8d24fdabee28da20d15
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-09-02 18:07:38 +0530

    Add files via upload

---
+++ assignmnt/AreaCalculator.java
@@ -0,0 +1,23 @@
+
+
+package com.cognizant.shapes;
+import java.util.Scanner;
+
+class Rectangle{
+	 
+		
+			
+		void calculateArea() {
+			System.out.println("The area of the rectangle is calculated using formula length*breadth");
+		
+}
+}
+public class AreaCalculator{
+	public static void main(String[] args) {
+	Rectangle obj =new Rectangle();
+	obj.calculateArea();
+	
+		
+	
+	}
+}
