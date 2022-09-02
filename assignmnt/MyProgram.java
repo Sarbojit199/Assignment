90eff2de520b37208073833a62617ab25f7d116c
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-09-02 18:25:31 +0530

    Add files via upload

---
+++ assignmnt/MyProgram.java
@@ -0,0 +1,10 @@
+package com.cognizant.shapes;
+class MyProgram{
+	public static void main(String[] args) {
+		System.out.println("My first java program");			//after removing the error
+	}
+	
+	
+
+	
+}
