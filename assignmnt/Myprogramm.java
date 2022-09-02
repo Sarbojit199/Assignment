3894a22e1369f101d755876cd965affa7ea1ef79
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-09-02 18:19:58 +0530

    Add files via upload

---
+++ assignmnt/Myprogramm.java
@@ -0,0 +1,8 @@
+package com.cognizant.shapes;
+
+public class Myprogramm {
+	public static void main(String[] args) {
+			DisplayMessage1 obj =new DisplayMessage1();		//making an object of the class from same package
+			
+			obj.printMessage();					//call the meathod of that class displayMessage1
+}}												//output-Hello My Message
